636f8a0cb7a826a0a6f1dfeb1679966601dde033
Soma Sarkadi Nagy <135955407+sarkadins@users.noreply.github.com>
2023-11-29 11:35:56 +0100

    get random oppoent cars

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -5,6 +5,7 @@ import Race from './components/Race';
 import { useState, useEffect } from 'react'
 import './App.css'
 import Allcars from './components/Allcars';
+import OppoentGarage from './components/OpponentGarage';
 
 
 function App() {
@@ -12,20 +13,17 @@ function App() {
 const [allCars, setAllCars] = useState([])
 
   useEffect(() => {
-    fetch('http://localhost:4000/cars')
+    fetch('http://localhost:4000/api/cars')
       .then((res) => res.json())
       .then((data) => setAllCars(data))
       .catch((err) => console.error('Error: ', err));
   }, []);
 
-
-
-
-
   return (
     <>
       <div>
         {allCars && <Allcars allcars={allCars} />}
+        {allCars && <OppoentGarage allCars={allCars} />}
       </div>
     </>
   );
