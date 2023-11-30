3135b3d6e75309bbd1d6cdca062f4b40c196f63f
Miskolczi Gergő <miskolczi19981201@gmail.com>
2023-11-30 11:41:47 +0100

    race component Working with fetch

--- client/vite-project/src/components/Race.jsx
+++ client/vite-project/src/components/Race.jsx
@@ -1,63 +1,70 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-
 const Race = () => {
   const trackLength = 1200;
-  const [cars, setCars] = useState([]);
+  const [opponentCars, setOpponentCars] = useState(null);
+  const [userCars, setUserCars] = useState(null);
+  const [raceResult, setRaceResult] = useState('');
 
+  const [allCars, setAllCars] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:4000/api/cars')
       .then((res) => res.json())
-      .then((data) => setCars(data))
+      .then((data) => setAllCars(data))
       .catch((err) => console.error('Error: ', err));
   }, []);
 
-
-  console.log(cars);
+  useEffect(() => {
+    if (allCars.length > 0) {
+      const randomCars = [...allCars].sort(() => 0.5 - Math.random());
+      const getOpponentCars = randomCars.slice(0, allCars.length / 2);
+      const getUserCars = randomCars.slice(allCars.length / 2);
+      setUserCars(getUserCars);
+      setOpponentCars(getOpponentCars);
+    }
+  }, [allCars]);
 
   function raceCars() {
-    if (cars.length < 2) {
+    if (!opponentCars || !userCars) {
       console.log('Loading cars...');
       return;
     }
+    const randomOppIndex = Math.ceil(Math.random() * opponentCars.length - 1);
+    const randomUserIndex = Math.ceil(Math.random() * userCars.length - 1)
 
-    const fixedCarIndex = 0;
-    const randomIndex = Math.floor(Math.random() * cars.length);
-    if (randomIndex === 0) {
-      randomIndex + 1;
-    }
-    console.log(randomIndex)
-    const AICar = cars[fixedCarIndex];
-    const randomCar = cars[randomIndex];
-    console.log(randomCar)
-    const aiAcceleration = ((AICar.top_speed * 1000) / 3600) / AICar.acceleration;
-    const randomAcceleration = ((randomCar.top_speed * 1000) / 3600) / randomCar.acceleration;
-  
-    const timeToFinishAICar = Math.sqrt(2 * trackLength / aiAcceleration)
-    const timeToFinishRandomCar = Math.sqrt(2 * trackLength / randomAcceleration)
-    console.log('AI', timeToFinishAICar)
-    console.log('Player', timeToFinishRandomCar)
+    const opponentCar = opponentCars[randomOppIndex];
+    const userCar = userCars[randomUserIndex];
 
+    const aiAcceleration = ((opponentCar.top_speed * 1000) / 3600) / opponentCar.acceleration;
+    const randomAcceleration = ((userCar.top_speed * 1000) / 3600) / userCar.acceleration;
+
+    const timeToFinishAICar = Math.sqrt(2 * trackLength / aiAcceleration);
+    const timeToFinishRandomCar = Math.sqrt(2 * trackLength / randomAcceleration);
+    console.log('AI', timeToFinishAICar);
+    console.log('Player', timeToFinishRandomCar);
+    console.log('opp', opponentCar)
+    console.log('user', userCar)
     if (timeToFinishAICar > timeToFinishRandomCar) {
-      return (`winner is PLAYER: ${randomCar.manufacturer}, winner time is: ${timeToFinishRandomCar}`)
+      setRaceResult(`Winner is PLAYER: ${userCar.manufacturer}, winner time is: ${timeToFinishRandomCar}`);
     } else if (timeToFinishAICar < timeToFinishRandomCar) {
-      return (`winner is AI: ${AICar.manufacturer}, winner time is: ${timeToFinishAICar}`)
+      setRaceResult(`Winner is AI: ${opponentCar.manufacturer}, winner time is: ${timeToFinishAICar}`);
     } else {
-      return (`its a tie`)
+      setRaceResult("It's a tie");
     }
-
   }
-  // raceCars()
 
   return (
     <div>
-      {raceCars()}
-    <Link to={'/api/garage'}><button>Garage</button></Link>
+      <button onClick={() => raceCars()}>Race</button>
+      <div>{raceResult}</div>
+      <Link to={'/garage'}>
+        <button>Garage</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Race;
 
